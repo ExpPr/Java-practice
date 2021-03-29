eafc25b8dcf94bf3dd09457c196cc1a592f43402
ExpPr <81138046+ExpPr@users.noreply.github.com>
2021-03-29 10:39:34 +0900

    Update 구구단.java

--- 구구단.java
+++ 구구단.java
@@ -18,7 +18,7 @@ public class practicefile {
 		//구구단 2*1 ~ 9*9
 		for (int i=1;i<=9;i++) {
 			for (int p=2;p<=9;p++) {
-				System.out.printf("%d x %d = %d   ",p,i,p*i);
+				System.out.printf("%d x %d = %d\",p,i,p*i);
 			}
 			System.out.println("");
 		}
