57753c493326a4a2f168a55d66b1080255a63a0f
ExpPr <81138046+ExpPr@users.noreply.github.com>
2021-03-29 10:02:20 +0900

    Switch 구문 이용 및 이용 안하고 등급나누기

---
+++ Switch.java
@@ -0,0 +1,30 @@
+public class practicefile {
+	
+	public static void main(String[] arge) {
+		
+		int score=89;
+		
+		switch (score/10) {
+			case 10 :
+			case 9 :
+				System.out.println("A");
+				break;
+			case 8 :
+				System.out.println("B");
+				break;
+			case 7 :
+				System.out.println("C");
+				break;
+			default :
+				System.out.println("D");
+			
+				
+		}
+		//두번째방법
+		char a=65;
+
+
+		System.out.println((score/10==10) ?(char) (a):(char)(a+(10-(score/10+1))));
+
+	}
+}
