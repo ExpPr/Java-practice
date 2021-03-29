3927b0fcdd19ece1206f1e17f8dfc1bbcb8b0fa6
ExpPr <81138046+ExpPr@users.noreply.github.com>
2021-03-29 11:15:19 +0900

    난수맞추기
    0~99사이 난수값을 맞추기.
    객체방식도 있으나 비객체방식도 있음. 
    Random r = new Random() 후 int p=r.NextInt(100) (이건 0~0.999999 난수후 100배 곱한걸 int화 함. 한번 더 자료형 변환 x)
    Math.random() 이것은 double형으로 0~0.999999.. 난수발생, 무조건 형변환 필수

---
+++ 난수맞추기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class practicefile {
+	
+	public static void main(String[] arge) { //static 객체없이 공통으로 사용함을 의미.
+		int p,k,count=0;
+		Random r=new Random(); //굳이 객체지향을 조금이라도 더 넣는 방법
+		//int k1=(int) (Math.random()*100); <- 아니면 이런식으로 비체식 Math.random 이용 이때 이 random은 0~0.9999999까지
+		Scanner sc=new Scanner(System.in);
+		p=r.nextInt(100);
+		System.out.println(p);
+		do {
+			System.out.print("정답은 ? ");
+			k=sc.nextInt();
+
+			
+			if (p>k) {
+				System.out.println("정답은 이것보단 높아요 ~ ");
+				count++;
+			}
+			else if (p<k){
+				System.out.println("정답은 이것보단 낮아요 ~ ");
+				count++;
+			}
+				
+		} while (p!=k);
+		
+		
+		System.out.printf("%d회만에 풀었어요!",count+1);
+
+		
+		
+	}
+}
+
+
+
