06d3084483d34e2ed11804f1f3855c657b64fd7f
ExpPr <81138046+ExpPr@users.noreply.github.com>
2021-04-05 11:41:26 +0900

    Array 메소드 수정본
    수정본 추가

--- Arrays 메소드.java
+++ Arrays 메소드.java
@@ -33,7 +33,7 @@ public class practicefile {
 		int rep[]= Arrays.copyOfRange(mat, 0, 3);//복사 메소드. 원본배열 mat의 인덱스가 0부터 2까지 총 3개를 복사함
 		System.out.println("복사본 "+Arrays.toString(rep));
 		
-		System.out.println(Arrays.toString(mat));//배열에 있는 숫자 데이터를 문자열로 바꾸어줌. 이때 대괄호 콤마는 자동임.
+		System.out.println(Arrays.toString(mat));//배열에 있는 숫자 데이터를 문자열로 바꾸어줌. 이때 대괄호 콤마는 자동임. 숫자도 가능하고 string계열도 가능하고 다양함
 		
 		Arrays.sort(mat);
 		Arrays.sort(mat,2,mat.length); //인덱스가 2부터 mat.length-1까지"만" 오름차순 정렬"
