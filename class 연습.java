707a9b75e836764365d1c4132d1e48e2156457bd
ExpPr <81138046+ExpPr@users.noreply.github.com>
2021-04-12 11:33:23 +0900

    class 연습
    연습

---
+++ class 연습.java
@@ -0,0 +1,113 @@
+package PracticeJava;
+import java.util.Scanner;
+class Student {
+	String name;//이름
+	int num;//학번;
+	int age;//나이;
+	
+	Student() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("이름 : ");
+		name=sc.next();
+		System.out.println("학번 : ");
+		num=sc.nextInt();
+		System.out.println("나이 : ");
+		age=sc.nextInt();
+		
+		System.out.println("이름 : "+name+"// 학번 : "+num+"// 나이 : "+age);
+		sc.close();
+	}
+}
+
+class Date {
+	int year,month,date;
+	String mon;
+	Date(int y,int m,int d) {
+		year=y;
+		month=m;
+		date=d;
+	}
+	
+	
+	
+	void print1() {
+		System.out.printf("%d.%d.%d\n",year,month,date);
+	}
+	void print2() {
+		switch (month) {
+		case 1:
+			mon="Jen";
+			break;
+		case 2:
+			mon="Feb";
+			break;
+		case 3:
+			mon="Mar";
+			break;
+		case 4:
+			mon="Apr";
+			break;
+		case 5:
+			mon="May";
+			break;
+		case 6:
+			mon="Jun";
+			break;
+		case 7:
+			mon="July";
+			break;
+		case 8:
+			mon="Aug";
+			break;
+		case 9:
+			mon="Sep";
+			break;
+		case 10:
+			mon="Oct";
+			break;
+		case 11:
+			mon="Nov";
+			break;
+		case 12:
+			mon="Dec";
+			break;
+		}
+		
+		
+		
+		
+	}
+}
+
+class Account {
+	int balance;//계좌있는돈
+	Account(int k) {
+		balance=k;
+		System.out.println("새로운 계좌가 생성되었습니다.");
+	}
+	Account() {
+		this(0);
+		System.out.println("새로운 계좌가 생성되었습니다.");
+	}
+
+	
+	void withdraw(int amount) {
+		balance-=amount;
+		System.out.println("계좌에 "+amount+"만큼 인출되었습니다");
+	}
+	void deposit(int amount) {
+		balance+=amount;
+		System.out.println("계좌에 "+amount+"만큼 입금되었습니다");
+	}
+}
+
+
+
+	
+	
+public class Practice {
+	public static void main(String args[]) {
+
+	}
+}
+
